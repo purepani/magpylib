1ba3d2cc17cf9f9a36c8e11ef18bbc30ab7d98f7
purepani <pani0028@umn.edu>
2025-05-06 11:12:13 -0500

    Modifies low level tests for array api

--- src/magpylib/_src/fields/special_elliptic.py
+++ src/magpylib/_src/fields/special_elliptic.py
@@ -11,6 +11,8 @@ def ellipeinc(phi: Array, m: Array):
 
 def ellipkinc(phi: Array, m: Array):
     return xpx.lazy_apply(sp.special.ellipkinc, phi, m, as_numpy=True)
+
+
 def ellipe(m: Array):
     return xpx.lazy_apply(sp.special.ellipe, m, as_numpy=True)
 
